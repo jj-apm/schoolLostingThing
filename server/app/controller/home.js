a3d5cdfbb11b90c04605857598927bd1b4fef5cb
jj-apm <jjj1431105276@gmail.com>
2020-01-05 23:02:08 +0800

    111

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -3,10 +3,10 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    const { ctx } = this;
-    ctx.body = 'hi, egg';
-  }
+    async index() {
+        const { ctx } = this;
+        ctx.body = 'hi, egg';
+    }
 }
 
 module.exports = HomeController;
